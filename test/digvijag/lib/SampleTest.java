44fd8d3bd296ef0720ad943ad27918f6dc6337a7
Digvijay <digvijag@thoughtworks.com>
2014-01-25 16:00:26 +0530

    sample test added

---
+++ test/digvijag/lib/SampleTest.java
@@ -0,0 +1,12 @@
+package digvijag.lib;
+
+import org.junit.Test;
+
+import static junit.framework.Assert.assertEquals;
+
+public class SampleTest {
+    @Test
+    public void test_one(){
+        assertEquals(1,1);
+    }
+}
